8f5c6b1c0671eb383e64c669d5880284b5e3e382
Muhammad Ashraf <33673504+mohameda2265@users.noreply.github.com>
2023-07-03 23:53:41 +0300

    Update settings.py

--- smart_soil/settings.py
+++ smart_soil/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$3#=1xofoaxm!+5ca5nb1-ljym*wc!-oizxmiiw_@z8eyj56rd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
