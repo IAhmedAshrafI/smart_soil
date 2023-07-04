ceb8e3432cd7d011b9c59b027e875b7f13a20330
Loay159 <84052550+Loay159@users.noreply.github.com>
2023-07-04 23:42:37 +0300

    edit in view

--- device/views.py
+++ device/views.py
@@ -1,19 +1,30 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Zone
+from .models import Zone, Zone_History
+from datetime import datetime
 
 
 
 
 class ZoneLevel(APIView):
-    def post(self, request, **kwargs):
+    def get(self, request, **kwargs):
         zones = Zone.objects.all()
+
+        error = {"error", "failed"}
+        if not zones and len(zones) < 10 :
+            return Response(error, status=status.HTTP_400_BAD_REQUEST)
+        if len(list(kwargs.values())) < 10:
+            return Response(error, status=status.HTTP_400_BAD_REQUEST)
 
         for value, zone in zip(kwargs.values(), zones):
             zone.level=value
-            print(zone.level)
+            zone_history = Zone_History()
+            zone_history.zone = zone
+            zone_history.level=value
+            zone_history.time = datetime.now()
             zone.save()
+            zone_history.save()
         data={"created":"success!"}
         return Response(data, status=status.HTTP_200_OK)
 
