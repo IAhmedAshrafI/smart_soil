2caadd6495d52879405022db59b915eadeb277b5
Loay159 <84052550+Loay159@users.noreply.github.com>
2023-07-08 05:02:50 +0300

    Update views.py

--- device/views.py
+++ device/views.py
@@ -1,8 +1,8 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import status
 from .models import Zone, Zone_History
 from datetime import datetime
-
-
 from django.http import HttpResponse
 import csv
 
@@ -12,23 +12,19 @@ class ZoneLevel(APIView):
         zones = Zone.objects.all()
 
         error = {"error", "failed"}
-        if not zones and len(zones) < 10 :
         if not zones and len(zones) < 10:
             return Response(error, status=status.HTTP_400_BAD_REQUEST)
         if len(list(kwargs.values())) < 10:
             return Response(error, status=status.HTTP_400_BAD_REQUEST)
 
         for value, zone in zip(kwargs.values(), zones):
-            zone.level=value
             zone.level = value
             zone_history = Zone_History()
             zone_history.zone = zone
-            zone_history.level=value
             zone_history.level = value
             zone_history.time = datetime.now()
             zone.save()
             zone_history.save()
-        data={"created":"success!"}
         data = {"created": "success!"}
         return Response(data, status=status.HTTP_200_OK)
 
