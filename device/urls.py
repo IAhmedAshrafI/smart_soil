ad03bc3d4607a8e4ea63415551c30654d133253e
Loay159 <84052550+Loay159@users.noreply.github.com>
2023-07-08 16:31:43 +0300

    Update urls.py

--- device/urls.py
+++ device/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from .views import ZoneLevel, ExportToCsv
 
 urlpatterns = [
-    path("device/<str:level1>/<str:level2>/<str:level3>/<str:level4>/<str:level5>/<str:level6>/<str:level7>/<str:level8>/<str:level9>/<str:level10>/",
+    path("device/<str:level1>/<str:level2>/<str:level3>/<str:level4>/<str:level5>/<str:level6>/<str:level7>/<str:level8>/<str:level9>/<str:level10>/"
+         <str:level11>/<str:level12>/<str:level13>/<str:level14>/<str:level15>/<str:level16>/<str:level17>/<str:level18>/<str:level19>/<str:level20>/,
          ZoneLevel.as_view(), name="zone-level"),
     path("exportcsv/", ExportToCsv.as_view(), name="exportcsv")
 ]
